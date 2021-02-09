c7d58cf0d688f8aa2f8db1d0344dcc823085b6f5
unknown <heen.ventura@hotmail.com>
2021-02-09 12:46:40 -0400

    Botones interactivos JS

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+function myFuncion () {
+    alert("En este momento las citas estan agotadas, regrese mas tarde.")
+    // document.write("Este boton fue precionado")
+}
+function myFuncion2 () {
+    alert("Aviso! no hay consulta, no joda!");
+}
